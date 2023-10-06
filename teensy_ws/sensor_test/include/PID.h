f247cb8b826ab61a6cef3feeb7e9e48d865476b3
BradenMeyers <113633773+BradenMeyers@users.noreply.github.com>
2023-10-06 10:23:11 -0600

    short term integral

--- teensy_ws/sensor_test/include/PID.h
+++ teensy_ws/sensor_test/include/PID.h
@@ -2,12 +2,15 @@
 #define PID
 
 #include <Arduino.h>
-#define INTEGRAL_CAP 4  //3 times the max output
+#define INTEGRAL_CAP 4  //4 times the max output
+#define INTEGRAL_ARRAY_SIZE 15 //How big the memory of the integral term
 
 class PID_Control
 {
 private:
     float integral = 0;
+    float integralArray[INTEGRAL_ARRAY_SIZE];
+    int integralIndex = 0;
     float error = 0;
     float kp = 0;
     float ki = 0;
@@ -23,16 +26,27 @@ public:
         min_output = min;
         max_output = max;
         bias = adjust;
-        Serial.println(p);
-        
-
+        // Initialize the integral array with zeros
+        for (int i = 0; i < INTEGRAL_ARRAY_SIZE; i++) {
+            integralArray[i] = 0;
+        }
     }
 
     float compute(float goal, float currentVal){
         error = goal - currentVal;
         Serial.print("Error: ");
         Serial.println(error);
-        integral = integral + error;
+
+        // Update the integral array, replacing the oldest value
+        integralArray[integralIndex] = error;
+        integralIndex = (integralIndex + 1) % INTEGRAL_ARRAY_SIZE;
+
+        //Have the integral be the sum of the error of the last 15 values
+        float integral = 0;
+        for (int i = 0; i < INTEGRAL_ARRAY_SIZE; i++) {
+            integral += integralArray[i];
+        }
+        //Cap the integral term 
         if(integral > ((max_output - bias)*INTEGRAL_CAP)){
             integral = (max_output - bias)*INTEGRAL_CAP;
         }
@@ -45,6 +59,8 @@ public:
         Serial.print("PID value");
         Serial.println(pidVal);
         float output = bias + pidVal;
+        
+        //Clamp the output to not allow it to exceed the limit
         if(output > max_output){
             output = max_output;
         }
