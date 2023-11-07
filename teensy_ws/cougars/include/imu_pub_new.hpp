f81aece02e0eded6d0bee42ac81baff07074d659
Nelson Durrant <snelsondurrant@gmail.com>
2023-11-07 20:51:45 +0000

    IMU updates

---
+++ teensy_ws/cougars/include/imu_pub_new.hpp
@@ -0,0 +1,23 @@
+#ifndef IMU_PUB_NEW
+#define IMU_PUB_NEW
+
+#include "publisher.hpp"
+#include <Adafruit_BNO08x.h>
+#include <Wire.h>
+#include <frost_interfaces/msg/imu.h>
+
+class IMUPubNew : Publisher {
+
+public:
+
+    void setup();
+  void imu_setup();
+  void imu_update();
+  void publish();
+  using Publisher::destroy;
+
+private:
+  frost_interfaces__msg__IMU msg;
+};
+
+#endif // IMU_PUB
